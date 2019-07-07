9fe730b9ab01f33bbd49966bd182b5c78a6fa95a
Irham Maulani <irhammaulani@gmail.com>
2019-07-08 00:11:02 +0800

    controller for main function add book

---
+++ app/Http/Controllers/BookImageController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\BookImage;
+use Illuminate\Http\Request;
+
+class BookImageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\BookImage  $bookImage
+     * @return \Illuminate\Http\Response
+     */
+    public function show(BookImage $bookImage)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\BookImage  $bookImage
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(BookImage $bookImage)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\BookImage  $bookImage
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, BookImage $bookImage)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\BookImage  $bookImage
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(BookImage $bookImage)
+    {
+        //
+    }
+}
