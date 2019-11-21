ef53897a0564b4a5e2f098dc8828e74392e76fb7
Irham Maulani <irhammaulani@gmail.com>
2019-11-21 23:44:07 +0800

    add BaseService that can be extended by another service class

---
+++ app/Services/BaseService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+class BaseService
+{
+    private $model;
+
+    public function __construct($model)
+    {
+        $this->model = $model;
+    }
+
+    public function setRelationship($relations)
+    {
+        $this->model = $this->model->with($relations);
+        return $this;
+    }
+
+    public function getDataPagination($data = 5)
+    {
+        return $this->model->paginate($data);
+    }
+
+    public function sortBy($order)
+    {
+        if ($order === null) $order = 'asc';
+
+        $this->model = $this->model->orderBy('created_at', $order);
+        return $this;
+    }
+
+    public function setValue($value)
+    {
+        $this->model = $this->mode->select($value);
+        return $this;
+    }
+
+    public function setSearch($search)
+    {
+        $this->model = $this->model->search($search);
+        return $this;
+    }
+}
