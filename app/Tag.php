82bd2d50771a35131a1738de776aa52f564fb994
Irham Maulani <irhammaulani@gmail.com>
2019-11-22 06:29:40 +0800

    add books relationship

--- app/Tag.php
+++ app/Tag.php
@@ -2,9 +2,14 @@
 
 namespace App;
 
+use App\Book;
 use Illuminate\Database\Eloquent\Model;
+
 
 class Tag extends Model
 {
-    //
+    public function books()
+    {
+        return $this->belongsToMany(Book::class);
+    }
 }
