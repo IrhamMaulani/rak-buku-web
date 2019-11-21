f5649518379b189d319a400cb1590fd5995c1791
Irham Maulani <irhammaulani@gmail.com>
2019-11-21 23:45:44 +0800

    add books relationship

--- app/Bookmark.php
+++ app/Bookmark.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Book;
 use Illuminate\Database\Eloquent\Model;
 
 class Bookmark extends Model
 {
-    //
+    public function book()
+    {
+        return $this->belongTo(Book::class);
+    }
 }
--- app/Score.php
+++ app/Score.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Book;
 use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    //
+    public function book()
+    {
+        $this->belongsTo(Book::class);
+    }
 }
