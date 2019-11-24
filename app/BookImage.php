66712159c02f380220f7aa064f084a70af4750f9
Irham Maulani <irhammaulani@gmail.com>
2019-11-24 22:40:57 +0800

    add book relationship

--- app/BookImage.php
+++ app/BookImage.php
@@ -2,9 +2,13 @@
 
 namespace App;
 
+use App\Book;
 use Illuminate\Database\Eloquent\Model;
 
 class BookImage extends Model
 {
-    //
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
