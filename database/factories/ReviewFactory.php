17cd1612745f20d5c6b8906ed7f39fafc3f11783
Irham Maulani <irhammaulani@gmail.com>
2019-11-25 01:58:51 +0800

    add reviewfactory

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -6,7 +6,15 @@ use App\Review;
 use Faker\Generator as Faker;
 
 $factory->define(Review::class, function (Faker $faker) {
+    $bookId = App\Book::pluck('id')->toArray();
+    $userId = App\User::pluck('id')->toArray();
+    $title = $faker->realText(15);
+    $slug = $title . ' ' . ' ' . $faker->randomElement($bookId) . ' ' . $faker->randomElement($userId);
     return [
-        //
+        'title' => $title,
+        'content' => $faker->realText(45),
+        'user_id' => $faker->randomElement($userId),
+        'book_id' => $faker->randomElement($bookId),
+        'slug' => str_slug($slug, '-')
     ];
 });
