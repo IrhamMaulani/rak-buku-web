4df65263f22d19efbaca91c081016a360addc8f5
Irham Maulani <irhammaulani@gmail.com>
2019-11-26 11:50:35 +0800

    add sync

--- app/Services/ReviewService.php
+++ app/Services/ReviewService.php
@@ -3,11 +3,12 @@
 namespace App\Services;
 
 use App\Review;
+use Illuminate\Support\Facades\DB;
 
 class ReviewService extends BaseService
 {
 
-    private $book;
+    private $review;
 
     public function __construct(Review $review)
     {
@@ -19,5 +20,47 @@ class ReviewService extends BaseService
     {
         return  $this->setRelationship(['user', 'book'])
             ->setScope('search', $search)->setScope('tag', $tag)->sortBy($sortBy)->getDataPagination($pagination);
+    }
+
+    public function syncAllResponse()
+    {
+        $reviews =  DB::table('reviews')
+            ->join('review_responses', 'reviews.id', '=', 'review_responses.review_id')
+            ->select(
+                'reviews.id',
+                DB::raw("(SELECT COUNT(review_responses.is_like) FROM review_responses WHERE
+                review_responses.review_id = reviews.id AND review_responses.is_like = 1 )
+                as likes"),
+                DB::raw("(SELECT COUNT(review_responses.is_like) FROM review_responses WHERE
+                review_responses.review_id = reviews.id AND review_responses.is_like = 0 )
+                as dislikes"),
+                DB::raw("(SELECT COUNT(review_responses.is_helpful) FROM review_responses WHERE
+                review_responses.review_id = reviews.id AND review_responses.is_helpful = 1 )
+                as helpful"),
+                DB::raw("(SELECT COUNT(review_responses.is_helpful) FROM review_responses WHERE
+                review_responses.review_id = reviews.id AND review_responses.is_helpful = 0 )
+                as not_helpful")
+            )
+            ->groupBy('reviews.id')
+            ->get();
+
+        try {
+            foreach ($reviews as $review) {
+                $reviewUpdate = $this->review->findOrFail($review->id);
+
+                $reviewUpdate->likes = $review->likes;
+
+                $reviewUpdate->dislikes = $review->dislikes;
+
+                $reviewUpdate->helpful = $review->helpful;
+
+                $reviewUpdate->not_helpful = $review->not_helpful;
+
+                $reviewUpdate->save();
+            }
+        } catch (\Throwable $th) {
+            dd($th);
+        }
+        return "Success";
     }
 }
