42d2ed3b574711a5e1f3f6d1ff063ee5f456ee42
Irham Maulani <irhammaulani@gmail.com>
2019-12-07 18:24:03 +0800

    drop i_favorite in score table and move to bookmarks

---
+++ database/migrations/2019_12_07_092426_drop_is_favorite_in_scores_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropIsFavoriteInScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('scores', function (Blueprint $table) {
+            $table->dropColumn('is_favorite');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('scores', function (Blueprint $table) {
+            //
+        });
+    }
+}
