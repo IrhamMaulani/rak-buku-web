f0754b433b73d25fdf7b15385607dc8c3e7ee389
Irham Maulani <irhammaulani@gmail.com>
2019-12-08 19:35:11 +0800

    add getAllData

--- app/Services/BookMarkService.php
+++ app/Services/BookMarkService.php
@@ -9,13 +9,40 @@ use App\Bookmark;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+
 class BookMarkService extends BaseService
 {
-    private $bookmark;
+    private $bookmark, $book;
 
-    public function __construct(Bookmark $bookmark)
+    public function __construct(Bookmark $bookmark, Book $book)
     {
         $this->bookmark = $bookmark;
+        $this->book = $book;
+        
+    }
+
+    public function getAllData(Request $request){
+        
+        $status = null;
+        $limit = 6;
+        $isOwned = null;
+        $isFavorite = null;
+        if ($request->has('status')) $status = $request->query('status');
+        if ($request->has('limit')) $limit = $request->query('limit');
+        if ($request->has('isOwned')) $isOwned = $request->query('isOwned');
+        if ($request->has('isFavorite')) $isFavorite = $request->query('isFavorite');
+
+          return  $this->book->whereHas('checkBookmarked', function ($query)  use ($status, $isOwned, $isFavorite){ 
+             if(!is_null($status)){
+              $query->whereStatus($status);
+                }
+              if(!is_null($isOwned)){
+              $query->whereIsOwned($isOwned);
+              }
+               if(!is_null($isFavorite)){
+              $query->whereIsFavorite($isFavorite);
+              }
+          })->with(['checkBookmarked','userScore','authors', 'bookImagesCover'])->orderBy('created_at', 'asc')->paginate($limit);
     }
 
     public function addBookMark(Request $request)
