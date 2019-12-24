740864cad8c5412b721e4db612d557b9bcc866e1
Irham Maulani <irhammaulani@gmail.com>
2019-12-24 21:35:27 +0800

    add updateData in authorService

--- app/Services/AuthorService.php
+++ app/Services/AuthorService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\User;
 use App\Author;
+use App\AuthorImage;
 use App\SocialMedia;
 use Illuminate\Http\Request;
 use App\Helpers\StringHelper;
@@ -37,7 +38,7 @@ class AuthorService extends BaseService
 
         return  $this->setScope('search', $search)
             ->setValue(['id', 'name', 'pen_name', 'birth_place', 'birth_date', 'residence_place', 'user_id'])
-            ->setRelationship(['user:id,name', 'authorImage'])
+            ->setRelationship(['user:id,name', 'authorImage', 'socialMedias'])
             ->orderBy($orderBy, $order)->getAllDatas();
     }
 
@@ -76,7 +77,54 @@ class AuthorService extends BaseService
                 }
             });
         } catch (\Throwable $th) {
-            return $th;
+            return 'Failed';
+        }
+        return "Success";
+    }
+
+    public function updateData(Request $request, $authorId)
+    {
+        try {
+            DB::transaction(function () use ($request, $authorId) {
+                $author = $this->author->findOrFail($authorId);
+
+                $author->name = $request->name;
+                $author->pen_name = $request->pen_name;
+                $author->birth_place = $request->birth_place;
+                $author->birth_date = $request->birth_date;
+                $author->residence_place = $request->residence_place;
+                $author->save();
+
+                if ($request->hasFile('author_images')) {
+                    $authorImagesName = $this->uploadHelper->uploadFile(
+                        $request->author_images,
+                        $request->name,
+                        'author_images'
+                    );
+                    $authorImages = [
+                        'name' => $authorImagesName,
+                        'user_id' =>   User::getAuthId(),
+                    ];
+
+                    if ($author->authorImage->name) {
+                        $this->uploadHelper->deleteFile($author->authorImage->name);
+                    }
+                    $authorImage = AuthorImage::whereAuthorId($author->id)->first();
+
+                    $authorImage->name = $authorImagesName;
+
+                    $authorImage->save();
+                }
+                foreach ($this->socialMedia->convertToInsertData($request->social_medias) as $socialMedia) {
+
+                    DB::table('author_social_media')
+                        ->updateOrInsert(
+                            ['author_id' => $author->id, 'social_media_id' => $socialMedia['id']],
+                            ['url' => $socialMedia['url']]
+                        );
+                }
+            });
+        } catch (\Throwable $th) {
             return 'Failed';
         }
         return "Success";
