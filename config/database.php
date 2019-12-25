13432cefef084c61b34219c05533ea93e001b03c
Irham Maulani <irhammaulani@gmail.com>
2019-12-26 01:18:51 +0800

    add deploy environment to heroku

--- config/database.php
+++ config/database.php
@@ -1,5 +1,8 @@
 <?php
+
+$DATABASE_URL = parse_url('postgres://jcppqoaopunkig:5ce3ff25c38ba1dbb3dce745733a00080ba277d71ae58581068fd2f5b35292c0@ec2-107-21-93-51.compute-1.amazonaws.com:5432/d669k1fsvrcs91');
 return [
+
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -10,7 +13,9 @@ return [
     | you may use many connections at once using the Database library.
     |
     */
-    'default' => env('DB_CONNECTION', 'postgres'),
+
+    'default' => env('DB_CONNECTION', 'pgsql'),
+
     /*
     |--------------------------------------------------------------------------
     | Database Connections
@@ -26,13 +31,16 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+
     'connections' => [
+
         'sqlite' => [
             'driver' => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
@@ -48,19 +56,20 @@ return [
             'strict' => false,
             'engine' => null,
         ],
+
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
+
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'host' => env('DB_HOST', 'localhost'),
@@ -72,7 +81,9 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
+
     ],
+
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
@@ -83,7 +94,9 @@ return [
     | the migrations on disk haven't actually been run in the database.
     |
     */
+
     'migrations' => 'migrations',
+
     /*
     |--------------------------------------------------------------------------
     | Redis Databases
@@ -94,19 +107,25 @@ return [
     | such as APC or Memcached. Laravel makes it easy to dig right in.
     |
     */
+
     'redis' => [
+
         'client' => 'predis',
+
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_DB', 0),
         ],
+
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => env('REDIS_CACHE_DB', 1),
         ],
+
     ],
+
 ];
