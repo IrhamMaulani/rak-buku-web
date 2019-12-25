2f7f30c1e5cec33162cec406e69c891291976289
Irham Maulani <irhammaulani@gmail.com>
2019-12-26 01:13:36 +0800

    fix insert image

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -19,44 +19,44 @@ class ProfileService extends BaseService
         $this->userImage = $userImage;
     }
 
-    public function getData($userName){
-        
+    public function getData($userName)
+    {
+
         return $this->user->with(['reputation', 'imageProfile'])->whereName($userName)->first();
     }
 
-    public function updateData(Request $request, $userName){
-         try {
+    public function updateData(Request $request, $userName)
+    {
+        try {
             DB::transaction(function () use ($request, $userName) {
-        $userId = $this->user->getAuthId();
-        $user = $this->user->whereName($userName)->whereId($userId)->first();
-        $user->full_name = $request->full_name;
-        $user->name = $request->name;
-        $user->email = $request->email;
+                $userId = $this->user->getAuthId();
+                $user = $this->user->whereName($userName)->whereId($userId)->first();
+                $user->full_name = $request->full_name;
+                $user->name = $request->name;
+                $user->email = $request->email;
+                $user->save();
 
-         if ($request->hasFile('image_profile')) {
-            $userImage = $this->userImage->whereUserId($userId)->whereIsUse(1)->first();
-                if($userImage){
-                    $userImage->is_use = 0;
-                    $userImage->save();
-                }
+                if ($request->hasFile('image_profile')) {
+                    $userImage = $this->userImage->whereUserId($userId)->whereIsUse(1)->first();
+                    if ($userImage) {
+                        $userImage->is_use = 0;
+                        $userImage->save();
+                    }
                     $profileImageName = $this->uploadHelper->uploadFile(
                         $request->image_profile,
                         $request->title,
                         'user_profiles'
                     );
-                    $profileImage = [
-                        'name' => $profileImageName,
-                        'user_id' =>  $userId,
-                        'is_use' => 1
-                    ];
-                $user->imageProfile()->create($profileImage);
-        }
-        $user->save();
-     });
-    } catch (\Throwable $th) {
+                    $user->imageProfile()->updateOrCreate(
+                        ['user_id' =>  $userId],
+                        ['name' => $profileImageName, 'is_use' => 1]
+                    );
+                }
+            });
+        } catch (\Throwable $th) {
             return $th;
             return 'Failed';
         }
         return "Success";
     }
-        }
+}
