20f680b6cc0ffb284426557619c4ffe8f8aac78f
Irham Maulani <irhammaulani@gmail.com>
2019-12-25 12:27:17 +0800

    add seeder for user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Role;
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,6 +13,38 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 25)->create();
+        if (DB::table('users')->get()->count() == 0) {
+            DB::table('users')->insert([
+                [
+                    'name' => 'moderator',
+                    'email' =>  'moderator@moderator.com',
+                    'reputation_id' => 1,
+                    'full_name' => 'moderator',
+                    'is_ban' => 1,
+                    'password' => bcrypt('moderator123456789'),
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+                [
+                    'user_name' => 'user',
+                    'email' =>  'user@user.com',
+                    'phone_number' => '08115169695',
+                    'reputation_id' => 1,
+                    'full_name' => 'user',
+                    'is_ban' => 1,
+                    'gender' => 'user',
+                    'password' => bcrypt('user123456789'),
+                    'created_at' => date('Y-m-d H:i:s'),
+                    'updated_at' => date('Y-m-d H:i:s'),
+                ],
+            ]);
+            $moderator = User::whereName('moderator')->first();
+            $moderator->roles()
+                ->sync([Role::whereName('moderator')->first()->id]);
+
+
+            $user = User::whereName('user')->first();
+            $user->roles()->sync(Role::whereName('member')->first()->id);
+        }
     }
 }
