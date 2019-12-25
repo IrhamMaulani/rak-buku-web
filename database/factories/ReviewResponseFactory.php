30464a0d7af9302d471e70459fccf2b58b4c967a
Irham Maulani <irhammaulani@gmail.com>
2019-12-25 14:40:29 +0800

    remove is_helpful in review_response factory

--- database/factories/ReviewResponseFactory.php
+++ database/factories/ReviewResponseFactory.php
@@ -10,7 +10,6 @@ $factory->define(ReviewResponse::class, function (Faker $faker) {
     $userId = App\User::pluck('id')->toArray();
 
     return [
-        'is_helpful' => $faker->randomElement([1, 0]),
         'user_id' => $faker->randomElement($userId),
         'review_id' => $faker->randomElement($reviewId),
         'is_like' =>  $faker->randomElement([1, 0])
